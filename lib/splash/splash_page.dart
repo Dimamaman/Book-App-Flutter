bbf930f8f2b250ad8401d1f969e77d889150f010
dinmuxammedmamanov <dimamanov37@gmail.com>
2023-08-20 16:51:35 +0500

    Provider added to MainScreen

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,14 +1,6 @@
-import 'dart:async';
-
-import 'package:book_app_flutter/main/main_screen.dart';
-import 'package:book_app_flutter/onboarding/onboarding_screen.dart';
-import 'package:book_app_flutter/pref/my_shared_pref.dart';
-import 'package:book_app_flutter/splash/splash_screen.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-import '../pref/book_pref.dart';
 
 class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
@@ -20,33 +12,21 @@ class Splash extends StatefulWidget {
 class _SplashState extends State<Splash> {
 
   bool? isFirst;
-  BookPRef sharedPreferencesManager = BookPRef();
-  final pref = MySharedPref();
 
   Future<void> next() async {
     await Future.delayed(const Duration(milliseconds: 2000));
   }
   Future<void> load() async {
     final SharedPreferences pref = await SharedPreferences.getInstance();
-    isFirst = pref.getBool('isOpened')!;
+    isFirst = pref.getBool('isFirst') ?? true;
   }
 
   @override
   void initState() {
-    print("initga kirdi");
-    // load();
-    // next().then((value) {
-    //   Navigator.pushReplacement(context, CupertinoPageRoute(builder: (context) => isFirst==null||isFirst == true ? Splash_Screen() : Main_Screen()));
-    // });
-
-    if(sharedPreferencesManager.getBool()) {
-      print("ifge kirdi");
-      Navigator.pushReplacement(context, CupertinoPageRoute(builder: (context) => OnBoarding_Screen()));
-    } else {
-      print("else kirdi");
-      Navigator.pushReplacement(context, CupertinoPageRoute(builder: (context) => Main_Screen()));
-    }
-
+    load();
+    next().then((value) {
+      isFirst == true ? Get.offNamed('/onboard') : Get.offNamed('/home');
+    });
     super.initState();
   }
 
@@ -55,8 +35,7 @@ class _SplashState extends State<Splash> {
     return Scaffold(
       body: Center(
         child: SizedBox(
-          height: 250,
-            width: 250,
+          height: 200,
             child: Image.asset('assets/images/splash.png')
         ),
       ),
